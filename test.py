ff6bfcf90615fb65ecf3cb76931c1b4145b70a2d
Dmitriy Krylov <kou5t@yandex.ru>
2023-05-18 06:00:54 +0300

    update variables

--- test.py
+++ test.py
@@ -5,23 +5,22 @@ import time
 
 
 def request_prices(url):
-    rs = requests.get(url)
-    m = re.search(r'var line1=(.+);', rs.text)
-    data_str = m.group(1)
-    data = json.loads(data_str)
+    response = requests.get(url)
+    result_search = re.search(r'var line1=(.+);', response.text).group(1)
+    prices = json.loads(result_search)
     file_name = url[url.rfind("/") + 1:]
-    with open(f'{file_name}.txt', 'w', encoding='UTF-8') as f:
-        f.write('ДАТА, ЦЕНА В $, КОЛИЧЕСТВО \n')
-        for line in data:
+    with open(f'{file_name}.txt', 'w', encoding='UTF-8') as file:
+        file.write('ДАТА, ЦЕНА В $, КОЛИЧЕСТВО\n')
+        for line in prices:
             data, price, value = line
-            f.write(f"'{data}', {price}, '{value}'\n")
-        print('Все успешно записано в файл: ' + file_name + '.txt')
+            file.write(f"'{data}', {price}, '{value}'\n")
+        print(f'Все успешно записано в файл: {file_name}.txt')
 
 
 def request_items():
-    rs = requests.get('https://steamcommunity.com/market/search?appid=730#p1_popular_desc')
-    urls = re.findall(r'<a class="market_listing_row_link" href="(.*)" id="resultlink_.">', rs.text)
-    for url in urls:
+    response = requests.get('https://steamcommunity.com/market/search?appid=730#p1_popular_desc')
+    result_search = re.findall(r'<a class="market_listing_row_link" href="(.*)" id="resultlink_.">', response.text)
+    for url in result_search:
         request_prices(url)
         time.sleep(5)
 
